a2857cd868a06be5a51c5293c11572ad409255da
CaptainOfPhB <captaininphw@gmail.com>
2021-03-16 23:59:11 +0800

    add contexts

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -3,83 +3,105 @@ import { MenuItem, Type } from '@/interface';
 const Menu: MenuItem[] = [
   {
     type: Type.Url,
-    title: '复制当前页面链接',
+    title: '复制页面链接',
     contexts: ['all']
   },
   {
     type: Type.Image,
     title: '复制图片链接',
-    contexts: ['link', 'image']
+    contexts: ['image']
   },
   {
     type: Type.Link,
     title: '复制链接地址',
-    contexts: ['link', 'image']
+    contexts: ['link']
+  },
+  {
+    type: Type.Separator
   },
   {
     type: Type.Markdown,
     title: '查看 Markdown 结构',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Html,
     title: '查看 HTML 结构',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
+  },
+  {
+    type: Type.Separator
   },
   {
     type: Type.Insert,
     title: '插入随机图片（Bing）',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Indent,
     title: '首行缩进',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Format,
     title: '文本格式化',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Directory,
     title: '复制目录列表',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.MindMapping,
     title: '生成思维导图',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.SerialNumber,
     title: '生成编号',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Save,
     title: '网页转存',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Diary,
     title: '日记',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
+  },
+  {
+    type: Type.Separator
   },
   {
     type: Type.Tooltip,
     title: '插入提示框',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Header,
     title: '插入彩色标题头',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   },
   {
     type: Type.Emoji,
     title: '插入表情',
-    onlyRunOnYuquePage: true
+    onlyRunOnYuquePage: true,
+    contexts: ['all']
   }
 ];
 
