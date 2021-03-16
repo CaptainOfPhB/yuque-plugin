76307ecc8a213c578d346d378bd5fabd3bea85bf
CaptainOfPhB <captaininphw@gmail.com>
2021-03-16 23:59:29 +0800

    add copyImage

--- src/content_scripts/messageListenerOfBackgroundScript.ts
+++ src/content_scripts/messageListenerOfBackgroundScript.ts
@@ -1,12 +1,21 @@
 import { Type } from '@/interface';
-import { copyUrl, viewMarkdown } from '@/actions';
+import MessageSender = chrome.runtime.MessageSender;
+import OnClickData = chrome.contextMenus.OnClickData;
+import { copyUrl, copyImage, viewMarkdown } from '@/actions';
 
-chrome.runtime.onMessage.addListener(function handleContextMenuClick(request: { type: Type }, _sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function handleContextMenuClick(
+  request: { type: Type; info: OnClickData },
+  _sender: MessageSender,
+  sendResponse
+) {
   console.log('yuque-plugin: receive message');
 
   switch (request.type) {
     case Type.Url:
       copyUrl();
+      break;
+    case Type.Image:
+      copyImage(request.info);
       break;
     case Type.Markdown:
       viewMarkdown();
