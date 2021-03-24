5ade95e358ba65a9c94731c9766db8a75fb29ed7
CaptainOfPhB <captaininphw@gmail.com>
2021-03-24 18:46:53 +0800

    change config stracture

--- src/config/defaultConfig.ts
+++ src/config/defaultConfig.ts
@@ -1,25 +1,34 @@
 import { Config } from '@/interface';
 
-const config: Config = {
-  CopyUrl: true,
-  CopyImage: true,
-  Indent: true,
-  CopyTOC: true,
-  Setting: true,
-  ViewHTML: true,
-  CopyLink: true,
-  CreateDiary: true,
-  InsertEmoji: true,
-  ViewMarkdown: true,
-  InsertTooltip: true,
-  CopyDirectory: true,
-  InsertBlankLine: true,
-  InsertRandomImage: true,
-  CreateMindMapping: true,
-  InsertSummaryInfo: true,
-  PlayAmbienceSound: true,
-  domain: 'www',
-  readingSpeed: 300
+const defaultConfig: Config = {
+  yuque: {
+    domain: 'www',
+    userName: undefined,
+    repoName: undefined,
+    accessToken: undefined
+  },
+  basic: {
+    readingSpeed: 300
+  },
+  menu: {
+    CopyUrl: true,
+    CopyImage: true,
+    Indent: true,
+    CopyTOC: true,
+    Setting: true,
+    ViewHTML: true,
+    CopyLink: true,
+    CreateDiary: true,
+    InsertEmoji: true,
+    ViewMarkdown: true,
+    InsertTooltip: true,
+    CopyDirectory: true,
+    InsertBlankLine: true,
+    InsertRandomImage: true,
+    CreateMindMapping: true,
+    InsertSummaryInfo: true,
+    PlayAmbienceSound: true
+  }
 };
 
-export default config;
+export default defaultConfig;
