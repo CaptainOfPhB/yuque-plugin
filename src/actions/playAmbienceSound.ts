5a86d94543b5ff04e3bf6f33212083b3f65b4c9d
CaptainOfPhB <captaininphw@gmail.com>
2021-04-12 23:47:12 +0800

    add player

--- src/actions/playAmbienceSound.ts
+++ src/actions/playAmbienceSound.ts
@@ -1,9 +1,21 @@
-import '@/player.less';
-
 function playAmbienceSound() {
-  const playerContainer = document.createElement('div');
-  playerContainer.classList.add('yuque-plugin__player-container');
-  document.body.append(playerContainer);
+  const container = document.createElement('div');
+  const button = document.createElement('div');
+  container.classList.add('yuque-plugin__player-container');
+  button.classList.add('yuque-plugin__player-button');
+  button.classList.add('yuque-plugin__player-paused');
+  container.append(button);
+  document.body.append(container);
+
+  const player = new Audio('http://music.163.com/song/media/outer/url?id=1824960995.mp3');
+  player.preload = 'auto';
+  player.loop = true;
+  void player.play();
+
+  button.addEventListener('click', function () {
+    button.classList.toggle('yuque-plugin__player-paused');
+    player.paused ? void player.play() : player.pause();
+  });
 }
 
 export default playAmbienceSound;
