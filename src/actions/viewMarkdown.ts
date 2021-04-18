60fa4ef829429e22c8bf6608bd19760e91c16b64
CaptainOfPhB <captaininphw@gmail.com>
2021-04-18 17:07:53 +0800

    remove redundant search parameters

--- src/actions/viewMarkdown.ts
+++ src/actions/viewMarkdown.ts
@@ -9,7 +9,7 @@ async function viewMarkdown() {
     return message.error('该功能只可在文档页面使用');
   }
   const [url] = window.location.href.split('/edit');
-  window.open(url.concat('/markdown?plain=true&linebreak=false&anchor=false'), '_blank');
+  window.open(url.concat('/markdown'), '_blank');
 }
 
 export default viewMarkdown;
